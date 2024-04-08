5f265d551801465021d80ac6455be1cea6202f7b
pixemisti <66703857+Ayabee527@users.noreply.github.com>
2024-04-08 13:38:29 +0000

    achievements start

---
+++ game/autoloads/AchievementHandler.gd
@@ -0,0 +1,70 @@
+extends Node
+
+const ACHIEVEMENTS = {
+	"Very Nice!": {
+		"completed": false,
+		"description": "Hit the goal 69 times in an endless run.",
+		"icon": "",
+	},
+	
+	"Voluntary Celibate!": {
+		"completed": false,
+		"description": "Don't hit the goal once in an endless run.",
+		"icon": "",
+	},
+	
+	"Okay, You're Pretty Good!": {
+		"completed": false,
+		"description": "Get a 25k+ score endless run.",
+		"icon": "",
+	},
+	
+	"Okay, You're Very Good!": {
+		"completed": false,
+		"description": "Get a 50k+ score endless run.",
+		"icon": "",
+	},
+	
+	"Okay, You're Too Good!": {
+		"completed": false,
+		"description": "Get a 75k+ score endless run.",
+		"icon": "",
+	},
+	
+	"Okay, You Can Stop Now!": {
+		"completed": false,
+		"description": "Get a 100k+ score endless run.",
+		"icon": "",
+	},
+	
+	"Tryhard!": {
+		"completed": false,
+		"description": "Get a 150k+ score endless run.",
+		"icon": "",
+	},
+	
+	"Maybe You Should Go Outside!": {
+		"completed": false,
+		"description": "Get a 200k+ score endless run.",
+		"icon": "",
+	},
+	
+	"Okay You're Pretty Bad!": {
+		"completed": false,
+		"description": "Finish an endless run with less than 1k score.",
+		"icon": "",
+	},
+	
+	"They're In The Walls!": {
+		"completed": false,
+		"description": "Hit the goal 10 times without touching the edges.",
+		"icon": "",
+	},
+	
+	"First Of Many!": {
+		"completed": false,
+		"description": "End your first endless run!",
+		"icon": "",
+	},
+	
+}
