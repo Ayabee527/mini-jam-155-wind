445416a8d783bf80d25b38c8bb87f586c6347fb0
pixemisti <66703857+Ayabee527@users.noreply.github.com>
2024-04-18 13:51:25 +0000

    gamemode select menu done

--- game/main_menu/mode_menu.gd
+++ game/main_menu/mode_menu.gd
@@ -1,35 +1,5 @@
 extends PanelContainer
 
-@export var description_label: RichTextLabel
-
-
-func set_description_text(text: String) -> void:
-	description_label.text = "[wave amp=16 freq=25][center]"
-	description_label.text += text
-
-func _on_endless_mouse_entered() -> void:
-	set_description_text("see how high you can get your highscore!")
-
-
-func _on_bullet_hell_mouse_entered() -> void:
-	set_description_text("see how long you can survive an onslaught!")
-
-
-func _on_rouge_like_mouse_entered() -> void:
-	set_description_text("coming soon!")
-
-
-func _on_endless_mouse_exited() -> void:
-	set_description_text("choose a gamemode!")
-
-
-func _on_bullet_hell_mouse_exited() -> void:
-	set_description_text("choose a gamemode!")
-
-
-func _on_rouge_like_mouse_exited() -> void:
-	set_description_text("choose a gamemode!")
-
 
 func _on_back_pressed() -> void:
 	hide()
@@ -40,8 +10,8 @@ func _on_endless_pressed() -> void:
 
 
 func _on_bullet_hell_pressed() -> void:
-	pass # Replace with function body.
+	print("bullet hel")
 
 
-func _on_rouge_like_pressed() -> void:
-	print("wait for 1.2 sweetie :3")
+func _on_rogue_lite_pressed() -> void:
+	print("Wait for 1.2 sweetie :3")
